e0670aa4ed9c1210e13078986300c96d1de95768
Anastasiu Tudor <anastasiu.tudor@gmail.com>
2022-01-24 18:11:46 +0200

    Updates

---
+++ script.js
@@ -0,0 +1,66 @@
+const form = document.querySelector('.form-container');
+const container = document.querySelector('.container');
+const input = document.querySelector('.add');
+
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if(input.value !== ""){
+        addItem(input.value);
+        input.value = "";
+    }
+    
+});
+
+const updateLS = () => {
+    const todos = document.querySelectorAll('.elem input');
+
+    const todosElements = [];
+    
+    todos.forEach(todo => todosElements.push(todo.value));
+
+    localStorage.setItem('todos', JSON.stringify(todosElements));
+}
+
+const addItem = (text="") => {
+    const todoEl = document.createElement('div');
+    todoEl.classList.add('elem');
+    todoEl.innerHTML = `
+        <input class="todoValue isDisable" type="text" autofocus>
+        <div class="buttons">
+        <button class="edit"><i class="fas fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+        </div>
+    `
+    const deleteBtn = todoEl.querySelector('.delete');
+    const editBtn = todoEl.querySelector('.edit');
+    const todoValue = todoEl.querySelector('.todoValue');
+   
+    todoValue.value = text
+    
+    deleteBtn.addEventListener('click', () => {
+        todoEl.remove();
+        updateLS();
+    });
+   
+    editBtn.addEventListener('click', (e) =>  {
+        todoValue.classList.toggle("isDisable");
+        todoValue.focus();
+    });
+    todoValue.addEventListener('input', (e) => {
+        const {value} = e.target;
+        todoValue.value = value;
+        todoValue.classList.add("isDisable");
+        updateLS();
+    })
+    container.appendChild(todoEl);
+    updateLS();
+}
+if(todos) {
+    todos.forEach(todo => {
+        addItem(todo);
+    })
+}
